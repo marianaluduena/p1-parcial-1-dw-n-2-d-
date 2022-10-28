d56b55bb1defd25928ed3e55295ebeae8c99e56d
Mariana Luduena <marianabelenluduena@gmail.com>
2022-10-27 21:34:57 -0300

    Agregadas funciones
    Se agregó cargarDuracionDelDisco, cargarOtraPista, cargarDatosDelDisco y mostrarDiscosCargados

--- js/programa.js
+++ js/programa.js
@@ -47,4 +47,107 @@ const cargarCodigoDelDisco = () => {
 // 4) PEDIR EL NOMBRE DE LA PISTA
 // 5) PEDIR DURACIÓN DE LA PISTA
 
-    
+
+const cargarDuracionDelDisco = () => {
+    let segundos = parseInt(prompt("Ingrese la duración de la pista medida en segundos"));
+
+    // Validación de la duración de la pista
+
+    if (segundos < 0 || segundos > 7200) {
+
+        mensaje = alert("La cantidad de segundos no puede ser menor a 0 ni mayor a 7200. Por favor ingrese un código válido");
+
+        // Se volverá a pedir al usuario que ingrese de nuevo el código
+        return cargarDuracionDelDisco();
+
+    }
+    return segundos;
+}
+
+// 6) PREGUNTAR AL USUARIO SI DESEA CARGAR OTRA PISTA
+
+function cargarOtraPista() {
+
+    // Pistas ya tiene previamente un dato cargado
+    pistas = [-1];
+
+    for (var i = 0; i < pistas.length; i++) {
+
+        let ingresarOtraPista = confirm("Desea agregar otra pista?");
+
+        if (!ingresarOtraPista) {
+            break;
+        }else{
+            pistas = pedirDato("Ingresa el nombre de la pista");
+            duracion = cargarDuracionDelDisco();
+        }
+        ingresarOtraPista = pistas[i];
+    }
+       return pistas;
+}
+
+
+
+function cargarDatosDelDisco() {
+
+    nombre = pedirDato("Ingrese el nombre del disco");
+    autor = pedirDato("Ingrese el autor del disco");
+    codigo = cargarCodigoDelDisco();
+    pistas = pedirDato("Ingresa el nombre de la pista");
+    duracion = cargarDuracionDelDisco();
+    cargarOtraPista();
+}
+
+function mostrarDiscosCargados() {
+
+    // Las variables ya tienen los datos del usuario
+
+    nombre;
+    autor;
+    codigo;
+    pistas;
+    duracion;
+
+    let disco = new DatosDelDisco(nombre, autor);
+    disco.codigo = codigo;
+    disco.pistas = pistas;
+    disco.duracion = duracion;
+
+    html = `<ul>
+    <li>${disco.nombre}</li>
+    <li>${disco.autor}</li>
+    <li>${disco.codigo}</li>
+    <li>${disco.pistas}</li>
+    <li>${disco.duracion}</li>
+   </ul>`;
+
+    document.getElementById("coleccion").innerHTML = html;
+}
+
+//............................................
+
+// PASOS A SEGUIR LUEGO DE CARGAR EL PRIMER DISCO:
+// PREGUNTAR AL USUARIO SI QUIERE CARGAR OTRO
+// SI ES ASÍ, VERIFICAR AL CARGAR EL CÓDIGO SI ÉSTE YA ESTÁ REPETIDO
+
+// Verificar si el código ingresado está repetido: cómo?
+//Opción 1: Comparar el array listaDeCodigos con una variable que verifique en un ciclo si el código está repetido o no
+
+/*
+for (let i = 0; i < listaDeCodigos.length; i++) {
+
+    const codigos = listaDeCodigos[i];
+    console.log(codigos);
+}*/
+
+/* USAR MÁS TARDE
+
+const verificarCodigoUnico = (verificarCodigo) => {
+    verificarCodigo = codigoDeldisco();
+
+    if (verificarCodigo === codigoDeldisco) {
+        alert("Código ya existente.");
+    }
+}*
+
+//verificarCodigoUnico();*/    
